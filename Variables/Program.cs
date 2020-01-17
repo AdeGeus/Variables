57525dc5fd67ecc579ee37df3d2b4d45d58ec25f
AdeGeus <60005233+AdeGeus@users.noreply.github.com>
2020-01-17 15:06:22 +0100

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstname;
+            myFirstname  = Console.ReadLine();
+
+            //string myLastname;
+            //Console.Write("Type your last name: ");
+            //myLastname = Console.ReadLine();
+
+            Console.WriteLine("Type your last name: ")
+            string myLastname = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstname + " " + myLastname);
+            Console.ReadLine();
+        }
+    }
+}
